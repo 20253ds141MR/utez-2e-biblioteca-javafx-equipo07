e7bdba39397e8561d4d4acd51b12ce72812584e3
Lian Joel Ramirez <lian@MacBook-Air-de-Lian.local>
2026-04-07 23:21:31 -0600

    refactor: eliminar codigo y archivos sin usar

--- src/main/java/com/example/utez2epacientesjavafxequipo07/model/Libro.java
+++ src/main/java/com/example/utez2epacientesjavafxequipo07/model/Libro.java
@@ -1,5 +1,4 @@
 package com.example.utez2epacientesjavafxequipo07.model;
-import java.util.Scanner;
 
 public class Libro {
     private String id;
@@ -22,51 +21,24 @@ public class Libro {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getTitulo() {
         return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
     }
 
     public int getAnio() {
         return anio;
     }
 
-    public void setAnio(int anio) {
-        this.anio = anio;
-    }
-
     public String getAutor() {
         return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
     }
 
     public String getGenero() {
         return genero;
     }
 
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
     public boolean isDisponible() {
         return disponible;
     }
 
-    public void setDisponible(boolean disponible) {
-        this.disponible = disponible;
-    }
-
-    public String[] split(String s) {
-        return s.split(",");
-    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,10 @@
 module com.example.utez2epacientesjavafxequipo07 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.desktop;
-
 
     opens com.example.utez2epacientesjavafxequipo07 to javafx.fxml;
     opens com.example.utez2epacientesjavafxequipo07.controllers to javafx.fxml;
-    opens com.example.utez2epacientesjavafxequipo07.services to javafx.fxml;
-    opens com.example.utez2epacientesjavafxequipo07.repositories to javafx.fxml;
 
     exports com.example.utez2epacientesjavafxequipo07;
     exports com.example.utez2epacientesjavafxequipo07.controllers;
-    exports com.example.utez2epacientesjavafxequipo07.services;
-    exports com.example.utez2epacientesjavafxequipo07.repositories;
 }
